b69a19ac76f732120c3f55ab0ee12af31213f63d
narankhuu <borgilchuluunnarankhuu746@gmail.com>
2022-12-31 15:05:51 +0800

    booking

--- routes/booking.js
+++ routes/booking.js
@@ -1,19 +1,47 @@
 import express from 'express';
 // import { Pool } from "pg";
+import pkg from 'pg';
+const {Pool} = pkg;
 const router = express.Router();
+// queries
+const queries = {
+    getTourId_Fname:`SELECT * FROM "Booking" where "TourId" = $1 AND "FName" = $2`,
+    postBooking:`INSERT INTO "Booking"("TourId", "FName", "Lname", "Date") VALUES ($1, $2, $3, $4);`
+}
+const pool = new Pool({
+    host: "localhost",
+    port: 3001,
+    database: "webbnt",
+    user: "postgres",
+    password: "admin"
+})
 // const router = require('express).Router();
-const returnSuccess = {"success": true};
-
-router.get('/', (req, res)=>{
-    res.send('Hello booking!');
-    // res.send()
-    // гэх байгаад query бичиж байгаад илгээнэ
+// Booking-ээс toir захиалсан cosID-тай хэрэглэгч
+router.get('/:TourId/:FName', (req, res)=>{
+    const params = req.params;
+    // console.log(params);
+    pool.query(queries.getTourId_Fname,[params.TourId, params.FName],(err, data)=>{
+        if(err) throw err;
+        if(data.rowCount){
+            res.status(200).json(data.rows);
+        }
+        else {
+            res.status(404).send(`${params.TourId}-тай tour ${params.FName} хэрэглэгч олдсонгүй`);   
+        }
+    })
 })
 router.post('/', (req, res)=>{
-    res.send('Hello booking post!');
-    // req.body/catogary_name 
-    // req-ээс орж ирснийг query бичиж оруулна
-    // res.status(201).send(returnSuccess);
+    const {
+        TourId,
+        FName,
+        Lname,
+        Date
+    } = req.body;
+    // console.log(params);
+    pool.query(queries.postBooking,[TourId, FName, Lname, Date],(err, data)=>{
+        res.status(201).send(`${TourId} ${FName} ${Lname} ${Date} created`);
+    })
 })
+
 
 export default router;
--- routes/tours.js
+++ routes/tours.js
@@ -8,8 +8,8 @@ const queries = {
     getTours:`SELECT * FROM "Tours"`,
     getToursId:`SELECT * FROM "Tours" where "TourId" = $1`,
     deleteToursId:`DELETE FROM "Tours" where "TourId" = $1`,
-    postTour:'INSERT INTO public."Tours"("TourId", "TourName", "Price") VALUES ($1, $2, $3);'
-    
+    postTour:'INSERT INTO "Tours"("TourId", "TourName", "Price") VALUES ($1, $2, $3);',
+    putTour:`UPDATE "Tours" SET "TourName"=$2, "Price"=$3 WHERE "TourId" = $1;`
 }
 // const router = require('express).Router();
 const returnSuccess = {"success": true};
@@ -21,11 +21,14 @@ const pool = new Pool({
     user: "postgres",
     password: "admin"
 })
+
+// tours table бүхэлд(get)
 router.get('/', (req, res)=>{
     pool.query(queries.getTours, (err, data)=>{
         res.status(200).json(data.rows);
     })
 })
+// tour id-гаар table-ээс(get)
 router.get('/:TourId', (req, res)=>{
     // req-ээс params-аас TourId-г авах
     let TourId = req.params.TourId;
@@ -34,29 +37,50 @@ router.get('/:TourId', (req, res)=>{
             res.status(200).json(data.rows);
         }
         else {
-            res.status(404).send("Not found");   
+            res.status(404).send(`${TourId} Not found`);   
         }
         // console.log(data);
         
     })
 })
+// tour id-гаар table-ээс(delete) хийх
 router.delete('/:TourId', (req, res)=>{
     // req-ээс params-аас TourId-г авах
     let TourId = req.params.TourId;
     pool.query(queries.getToursId,[TourId] ,(err, data)=>{
         if(data.rowCount){
             pool.query(queries.deleteToursId,[TourId],(err,data)=>{
-                res.status(200).send("DELETED");
+                res.status(200).send(`${TourId} DELETED`);
             })
         }
         else {
-            res.status(200).send("NOT REGISTERED TOUR ID");   
+            res.status(200).send(`${TourId} NOT REGISTERED TOUR ID`);   
         }
         // console.log(data);
         
     })
 })
-
+// tour id-гаар table-ээс(put||update)
+router.put('/', (req, res)=>{
+    // req-ээс params-аас TourId-г авах
+    const {
+        TourId,
+        TourName,
+        Price
+    } = req.body;
+    pool.query(queries.getToursId,[TourId], (err, data)=>{
+        // эхлээд оруулсан tourId -г бүртгэгдсэн эсэхийг шалгана
+        if(data.rowCount){
+            pool.query(queries.putTour, [TourId,TourName, Price], (err, data)=>{
+                res.status(200).send(`${TourId} uptaded`);
+            })
+        }
+        else{
+            res.send(`${TourId} NOT REGISTERED TOUR ID`);
+        }
+    })
+})
+// database -руугаа оруулах(post)
 router.post('/', (req, res)=>{
     const {
         TourId,
@@ -69,15 +93,14 @@ router.post('/', (req, res)=>{
     pool.query(queries.getToursId,[TourId], (err, data)=>{
         // эхлээд оруулсан tourId -г бүртгэгдсэн эсэхийг шалгана
         if(data.rowCount){
-            res.send("registered");
+            res.send(`${TourId} registered`);
         }
         else{
             pool.query(queries.postTour, [TourId,TourName,Price],(err,data)=>{
-                res.status(201).send("created");
+                res.status(201).send(`${TourId} created`);
             })
         }
     })
     
 })
-
 export default router;
